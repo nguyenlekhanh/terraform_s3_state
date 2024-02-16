3e16ce01894b8321baabb37b84d7321dddffc7c0
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-02-16 11:20:51 -0600

    save terra_state to s3 not to local

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 terraform {
   # Run init/plan/apply with "backend" commented-out (ueses local backend) to provision Resources (Bucket, Table)
   # Then uncomment "backend" and run init, apply after Resources have been created (uses AWS)
-#   backend "s3" {
-#     bucket         = "cc-tf-state-backend-ci-cd-999"
-#     key            = "tf-infra/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform-state-locking"
-#     encrypt        = true
-#   }
+  backend "s3" {
+    bucket         = "cc-tf-state-backend-ci-cd-999"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
 
   required_providers {
     aws = {
